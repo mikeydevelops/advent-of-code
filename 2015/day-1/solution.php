0d3733e1a497aec06bffca3447d18f4903ff1972
Michael Goodman <bulgedition@gmail.com>
2022-12-28 19:44:58 +0200

    solved second part of 2015-12-01

--- 2015/day-1/solution.php
+++ 2015/day-1/solution.php
@@ -5,19 +5,21 @@ require_once __DIR__ . '/../../common.php';
 $input = getInput();
 
 $floor = 0;
+$basementPosition = 0;
 
-foreach (str_split($input) as $instruction) {
+foreach (str_split($input) as $idx => $instruction) {
     if ($instruction == '(') {
         $floor ++;
-
-        continue;
     }
 
     if ($instruction == ')') {
         $floor --;
+    }
 
-        continue;
+    if ($basementPosition === 0 && $floor == -1) {
+        $basementPosition = $idx + 1;
     }
 }
 
-print("Santa will need to go to floor: $floor");
+print("1. Santa will need to go to floor: $floor");
+print("2. The position of the character that causes Santa to enter the basement is: $basementPosition");
