4531c62db7bbc154eb7ff134070203144a8125ae
Michael Goodman <bulgedition@gmail.com>
2022-12-28 21:32:46 +0200

    made solution be scoped in a function and only run it automatically if it was directly executed

--- 2015/day-1/solution.php
+++ 2015/day-1/solution.php
@@ -2,24 +2,33 @@
 
 require_once __DIR__ . '/../../common.php';
 
-$input = getInput();
+function aoc2015day1()
+{
+    $input = getInput();
 
-$floor = 0;
-$basementPosition = 0;
+    $floor = 0;
+    $basementPosition = 0;
 
-foreach (str_split($input) as $idx => $instruction) {
-    if ($instruction == '(') {
-        $floor ++;
+    foreach (str_split($input) as $idx => $instruction) {
+        if ($instruction == '(') {
+            $floor ++;
+        }
+
+        if ($instruction == ')') {
+            $floor --;
+        }
+
+        if ($basementPosition === 0 && $floor == -1) {
+            $basementPosition = $idx + 1;
+        }
     }
 
-    if ($instruction == ')') {
-        $floor --;
-    }
-
-    if ($basementPosition === 0 && $floor == -1) {
-        $basementPosition = $idx + 1;
-    }
+    return [$floor, $basementPosition];
 }
 
-line("1. Santa will need to go to floor: $floor");
-line("2. The position of the character that causes Santa to enter the basement is: $basementPosition");
+if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
+    list($floor, $basementPosition) = aoc2015day1();
+
+    line("1. Santa will need to go to floor: $floor");
+    line("2. The position of the character that causes Santa to enter the basement is: $basementPosition");
+}
