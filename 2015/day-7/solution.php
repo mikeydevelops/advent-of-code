91bc6597ada301fef40fa48428652a6a88606170
Michael Goodman <bulgedition@gmail.com>
2023-01-08 21:52:03 +0200

    added solution part one for 2015-12-07

---
+++ 2015/day-7/solution.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once __DIR__ . '/../../common.php';
+require_once __DIR__ . '/LogicGate.php';
+require_once __DIR__ . '/Wire.php';
+
+/**
+ * Get the instructions parsed instructions.
+ *
+ * @return \LogicGate[]
+ * @throws \Exception
+ */
+function getInstructions()
+{
+    $instructions = explode_trim("\n", getInput());
+
+    return array_map(function ($instruction) {
+        $tokens = explode(' ', $instruction);
+        $tokenLen = count($tokens);
+        $out = $in1 = $in2 = $op = null;
+
+        if ($tokenLen == 5) { // Full expression
+            $in1 = Wire::make(array_shift($tokens));
+            $op = array_shift($tokens);
+            $in2 = Wire::make(array_shift($tokens));
+        }
+
+        if ($tokenLen == 4) { // NOT
+            $op = array_shift($tokens);
+            $in1 = Wire::make(array_shift($tokens));
+        }
+
+        if ($tokenLen == 3) { // assignment
+            $op = 'ASSIGN';
+            $in1 = Wire::make(array_shift($tokens));
+        }
+
+        $out = Wire::wire(array_pop($tokens));
+
+        $gate = new LogicGate($op, $out, $in1, $in2);
+
+        $gate->setRaw($instruction);
+
+        return $gate;
+    }, $instructions);
+}
+
+/**
+ * Process the queue of logic gates.
+ *
+ * @param  \LogicGate[]  $gates
+ * @return void
+ */
+function processGates(array $gates)
+{
+    foreach ($gates as $idx => $gate) {
+
+        // line($gate);
+        if ($gate->ready()) {
+            $gate->eval();
+
+            // line($gate);
+
+            unset($gates[$idx]);
+
+            return processGates($gates);
+        }
+    }
+}
+
+/**
+ * Advent of Code 2015
+ * Day 7: Some Assembly Required
+ * Part One
+ *
+ * @return integer
+ * @throws \Exception
+ */
+function aoc2015day7part1()
+{
+    $gates = getInstructions();
+
+    processGates($gates);
+
+    return Wire::make('a')->getSignal();
+}
+
+if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
+    $signal = aoc2015day7part1();
+
+    line("1. The signal provided to wire a is: $signal");
+}
