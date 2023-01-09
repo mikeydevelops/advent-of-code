5e254570f724cd42f3edf4475e230cc088b2846b
Michael Goodman <bulgedition@gmail.com>
2023-01-09 19:59:39 +0200

    added description of part one for 2015-12-09

---
+++ 2015/day-9/solution.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . '/../../common.php';
+
+/**
+ * Advent of Code 2015
+ * Day 9: All in a Single Night
+ * Part One
+ *
+ * @return integer
+ * @throws \Exception
+ */
+function aoc2015day9part1(): int
+{
+    $input = getInput();
+
+    $distance = 0;
+
+    return $distance;
+}
+
+if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
+    $distance = aoc2015day8part1();
+
+    line("1. The shortest distance is: $distance");
+}
