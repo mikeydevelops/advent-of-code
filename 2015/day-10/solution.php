f724d5da7489a4b332c773c6b2252240957c04d5
Michael Goodman <bulgedition@gmail.com>
2023-01-09 22:14:24 +0200

    added solution of part one for 2015-12-10

--- 2015/day-10/solution.php
+++ 2015/day-10/solution.php
@@ -3,6 +3,38 @@
 require_once __DIR__ . '/../../common.php';
 
 /**
+ * Run the look-and-say algorithm on given string.
+ *
+ * @param  string  $input
+ * @param integer  $repeat
+ * @return string
+ */
+function lookAndSay(string $input) : string
+{
+    $output = '';
+
+    $tokens = [];
+
+    $lastToken = null;
+
+    foreach (str_split($input) as $token) {
+        if (!$lastToken || $lastToken[0] != $token) {
+            $tokens[] = [$token, 0];
+
+            $lastToken = &$tokens[count($tokens) - 1];
+        }
+
+        $lastToken[1] ++;
+    }
+
+    foreach ($tokens as $token) {
+        $output .= $token[1] . $token[0];
+    }
+
+    return $output;
+}
+
+/**
  * Advent of Code 2015
  * Day 10: Elves Look, Elves Say
  * Part One
@@ -13,8 +45,13 @@ require_once __DIR__ . '/../../common.php';
 function aoc2015day10part1(): int
 {
     $input = getInput();
+    $output = $input;
 
-    return 0;
+    foreach (range(1, 40) as $i) {
+        $output = lookAndSay($output);
+    }
+
+    return strlen($output);
 }
 
 if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
