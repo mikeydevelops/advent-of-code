03e463f9f361a1640eccf4409fece40198238d26
Michael Goodman <bulgedition@gmail.com>
2023-01-10 22:06:11 +0200

    added solution of part one for 2015-12-12

--- 2015/day-12/solution.php
+++ 2015/day-12/solution.php
@@ -14,7 +14,9 @@ function aoc2015day12part1(): int
 {
     $document = getInput();
 
-    return 0;
+    preg_match_all('/[+-]?((\d*\.?\d+)|(\d+\.?\d*))/', $document, $matches);
+
+    return array_sum($matches[0]);
 }
 
 if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
