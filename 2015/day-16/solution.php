1265bd57d4123ad5a8aecfbbcfdfa30a05972d1f
Michael Goodman <bulgedition@gmail.com>
2023-01-13 22:41:08 +0200

    added solution of part one for 2015-12-16

--- 2015/day-16/solution.php
+++ 2015/day-16/solution.php
@@ -3,6 +3,31 @@
 require_once __DIR__ . '/../../common.php';
 
 /**
+ * Get all Aunt Sue samples.
+ *
+ * @return array
+ * @throws \Exception
+ */
+function getAunts() : array
+{
+    $aunts = [];
+
+    foreach (explode("\n", getInput()) as $line) {
+        [$name, $compounds] = explode(': ', $line, 2);
+
+        $compounds = array_sliding(explode(' ', str_replace([':', ','], '', $compounds)), 2, 2);
+        $compounds = array_combine(array_column($compounds, 0), array_column($compounds, 1));
+        $compounds = array_map('intval', $compounds);
+
+        ksort($compounds);
+
+        $aunts[$name] = $compounds;
+    }
+
+    return $aunts;
+}
+
+/**
  * Advent of Code 2015
  * Day 16: Aunt Sue
  * Part One
@@ -12,13 +37,40 @@ require_once __DIR__ . '/../../common.php';
  */
 function aoc2015day16part1(): int
 {
-    $aunts = explode("\n", getInput());
+    $aunts = getAunts();
 
-    return 0;
+    $tickerTape = [
+        'children' => 3,
+        'cats' => 7,
+        'samoyeds' => 2,
+        'pomeranians' => 3,
+        'akitas' => 0,
+        'vizslas' => 0,
+        'goldfish' => 5,
+        'trees' => 3,
+        'cars' => 2,
+        'perfumes' => 1,
+    ];
+
+    ksort($tickerTape);
+
+    $auntNo = 0;
+
+    foreach ($aunts as $aunt => $compounds) {
+        $match = array_intersect_key($tickerTape, $compounds);
+
+        if ($match == $compounds) {
+            $auntNo = intval(substr($aunt, strpos($aunt, ' ')));
+
+            break;
+        }
+    }
+
+    return $auntNo;
 }
 
 if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
     $auntNo = aoc2015day16part1();
 
-    line("1. The aunt that gave me the gift is aunt #$auntNo");
+    line("1. The aunt that gave me the gift is Sue: $auntNo");
 }
