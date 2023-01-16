9dfda4e1ae29ccabe6fa6830348bb12f985e7424
Michael Goodman <bulgedition@gmail.com>
2023-01-16 19:58:52 +0200

    added solution of part two for 2015-12-19

--- 2015/day-19/solution.php
+++ 2015/day-19/solution.php
@@ -23,6 +23,23 @@ function getReplacementMap(string $replacements) : array
 }
 
 /**
+ * Reverse the replacements.
+ *
+ * @param  array  $replacements
+ * @return array
+ */
+function reverseReplacements(array $replacements): array
+{
+    $reversed = [];
+
+    foreach ($replacements as $search => $rep) {
+        $reversed = array_merge($reversed, array_combine($rep, array_fill(0, count($rep), $search)));
+    }
+
+    return $reversed;
+}
+
+/**
  * Advent of Code 2015
  * Day 19: Medicine for Rudolph
  * Part One
@@ -56,8 +73,44 @@ function aoc2015day19part1(): int
     return count($molecules);
 }
 
+
+/**
+ * Advent of Code 2015
+ * Day 19: Medicine for Rudolph
+ * Part One
+ *
+ * @return integer
+ * @throws \Exception
+ */
+function aoc2015day19part2(): int
+{
+    [$replacements, $molecule] = explode("\n\n", getInput());
+    $replacements = getReplacementMap($replacements);
+    $reversed = reverseReplacements($replacements);
+
+    $steps = 0;
+
+    $target = $molecule;
+
+    do {
+        foreach ($reversed as $search => $replace) {
+            if (($pos = strpos($target, $search)) === false) {
+                continue;
+            }
+
+            $target = preg_replace('/' . $search . '/', $replace, $target, 1);
+
+            $steps++;
+        }
+    } while ($target != 'e');
+
+    return $steps;
+}
+
 if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
     $molecules = aoc2015day19part1();
-
     line("1. The total number of unique molecules is: $molecules");
+
+    $steps = aoc2015day19part2();
+    line("2. The fewest number of steps to from e to the medicine molecule is: $steps");
 }
