74a196159dc92513807b6ba6b36f8386d5120b38
Michael Goodman <bulgedition@gmail.com>
2023-01-16 19:16:28 +0200

    added solution of part one for 2015-12-19

--- 2015/day-19/solution.php
+++ 2015/day-19/solution.php
@@ -3,6 +3,26 @@
 require_once __DIR__ . '/../../common.php';
 
 /**
+ * Get the replacement map for all molecules.
+ *
+ * @param  string  $replacements
+ * @return array
+ */
+function getReplacementMap(string $replacements) : array
+{
+    $replacements = array_filter(explode("\n", $replacements));
+    $map = [];
+
+    foreach ($replacements as $replacement) {
+        [$search, $replace] = explode(' => ', $replacement);
+
+        $map[$search][] = $replace;
+    }
+
+    return $map;
+}
+
+/**
  * Advent of Code 2015
  * Day 19: Medicine for Rudolph
  * Part One
@@ -12,9 +32,28 @@ require_once __DIR__ . '/../../common.php';
  */
 function aoc2015day19part1(): int
 {
-    $strings = explode("\n", getInput());
+    [$replacements, $molecule] = explode("\n\n", getInput());
+    $replacements = getReplacementMap($replacements);
 
-    return 0;
+    $molecules = [];
+
+    foreach (str_split($molecule) as $i => $element) {
+        foreach ($replacements as $search => $rep) {
+            $searchLen = strlen($search);
+
+            foreach ($rep as $replacement) {
+                if (substr($molecule, $i, $searchLen) != $search) {
+                    continue;
+                }
+
+                $new = substr_replace($molecule, $replacement, $i, $searchLen);
+
+                $molecules[$new] = 0;
+            }
+        }
+    }
+
+    return count($molecules);
 }
 
 if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
