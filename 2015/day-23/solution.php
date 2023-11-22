e92eac84437c1f5974df1fba7ec542f3c219409b
Michael Goodman <bulgedition@gmail.com>
2023-11-23 01:36:16 +0200

    added solution of part one for 2015-12-23

--- 2015/day-23/solution.php
+++ 2015/day-23/solution.php
@@ -7,11 +7,74 @@ require_once __DIR__ . '/../../common.php';
  * Day 23: Opening the Turing Lock
  * Part One
  *
- * @return string
+ * @return integer
  */
-function aoc2015day23part1(): string
+function aoc2015day23part1(): int
 {
-    throw new Exception('Not implemented.');
+    $instructions = explode("\n", getInput());
+
+    $registers = [];
+
+    $idx = 0;
+
+    $count = count($instructions);
+
+    while ($idx < $count) {
+        $instruction = explode(' ', $instructions[$idx]);
+
+        if (count($instruction) < 2) {
+            continue;
+        }
+
+        $cmd = $instruction[0];
+        $register = $cmd != 'jmp' ? rtrim($instruction[1], ',') : null;
+        $offset = intval($cmd == 'jmp' ? $instruction[1] : ($instruction[2] ?? 0));
+
+        if ($register && ! isset($registers[$register])) {
+            $registers[$register] = 0;
+        }
+
+        if ($cmd == 'jmp') {
+            $idx += $offset;
+
+            continue;
+        }
+
+        if ($cmd == 'jie') {
+            $idx += ($registers[$register] % 2) == 0 ? $offset : 1;
+
+            continue;
+        }
+
+        if ($cmd == 'jio') {
+            $idx += $registers[$register] == 1 ? $offset : 1;
+
+            continue;
+        }
+
+        if ($cmd == 'hlf') {
+            $registers[$register] = intval($registers[$register] / 2);
+            $idx += 1;
+
+            continue;
+        }
+
+        if ($cmd == 'tpl') {
+            $registers[$register] *= 3;
+            $idx += 1;
+
+            continue;
+        }
+
+        if ($cmd == 'inc') {
+            $registers[$register] ++;
+            $idx += 1;
+
+            continue;
+        }
+    }
+
+    return $registers['b'];
 }
 
 if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
