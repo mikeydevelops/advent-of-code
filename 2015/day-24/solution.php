931a3e8932cdc386e141bc959f40b4aa5bc69900
Michael Goodman <bulgedition@gmail.com>
2023-11-23 20:00:51 +0200

    added description of part one for 2015-12-24

---
+++ 2015/day-24/solution.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Advent of Code 2015
+ * Day 24: It Hangs in the Balance
+ *
+ * @return array
+ */
+function aoc2015day24(): array
+{
+    return [];
+}
+
+if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
+    $part1 = aoc2015day24()[0];
+
+    line("1. The quantum entanglement in the first group is: $part1.");
+}
