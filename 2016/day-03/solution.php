24adaf1b86fde5a3b0ca93a211208fd37f2d3ad4
Michael Goodman <bulgedition@gmail.com>
2023-11-25 21:14:50 +0200

    added solution of part one for 2016-12-03

--- 2016/day-03/solution.php
+++ 2016/day-03/solution.php
@@ -1,6 +1,20 @@
 <?php
 
 /**
+ * Get the impossible triangles.
+ *
+ * @return array<array<integer>>
+ */
+function getTriangles(): array
+{
+    $triangles = explode("\n", getInput());
+
+    return array_map(function ($triangle) {
+        return array_map('intval', preg_split('/\s+/', trim($triangle)));
+    }, $triangles);
+}
+
+/**
  * Advent of Code 2016
  * Day 3: Squares With Three Sides
  *
@@ -10,7 +24,21 @@
  */
 function aoc2016day3part1(): int
 {
+    $triangles = getTriangles();
+
     $possibilities = 0;
+
+    foreach ($triangles as $triangle) {
+        $pairs = [
+            $triangle[0] + $triangle[1] > $triangle[2],
+            $triangle[1] + $triangle[2] > $triangle[0],
+            $triangle[2] + $triangle[0] > $triangle[1],
+        ];
+
+        if (count(array_unique($pairs)) == 1 && $pairs[0] === true) {
+            $possibilities ++;
+        }
+    }
 
     return $possibilities;
 }
