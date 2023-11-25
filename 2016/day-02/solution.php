897904da72a29196f2c15c2d41d3b61fafc3fad4
Michael Goodman <bulgedition@gmail.com>
2023-11-25 10:11:54 +0200

    added description of part one for 2016-12-02

---
+++ 2016/day-02/solution.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Advent of Code 2016
+ * Day 2: Bathroom Security
+ *
+ * @return int
+ */
+function aoc2016day2(): int
+{
+    $code = 0000;
+
+    return $code;
+}
+
+if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
+    $code = aoc2016day2();
+
+    line("1. The bathroom code is: $code.");
+}
