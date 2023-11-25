8dc5554408ea0e40a7dcef050b630b87c833358d
Michael Goodman <bulgedition@gmail.com>
2023-11-25 11:03:50 +0200

    added solution of part two for 2016-12-02

--- 2016/day-02/solution.php
+++ 2016/day-02/solution.php
@@ -24,9 +24,11 @@ function getInstructions(bool $example = false): array
  * Advent of Code 2016
  * Day 2: Bathroom Security
  *
+ * Part One
+ *
  * @return int
  */
-function aoc2016day2(): int
+function aoc2016day2part1(): int
 {
     $code = '';
 
@@ -57,8 +59,56 @@ function aoc2016day2(): int
     return intval($code);
 }
 
+/**
+ * Check to see if given coordinates result in an edge for given keypad.
+ */
+function isAtEdge(array $keypad, int $x, int $y): bool
+{
+    return !isset($keypad[$y][$x]) || $keypad[$y][$x] === 0;
+}
+
+/**
+ * Advent of Code 2016
+ * Day 2: Bathroom Security
+ *
+ * Part Two
+ *
+ * @return string
+ */
+function aoc2016day2part2(): string
+{
+    $code = '';
+
+    $instructions = getInstructions();
+
+    $keypad = [
+        [0,  0,   1,   0,  0],
+        [0,  2,   3,   4,  0],
+        [5,  6,   7,   8,  9],
+        [0, 'A', 'B', 'C', 0],
+        [0,  0,  'D',  0,  0],
+    ];
+    $x = 0;
+    $y = 2;
+
+    foreach ($instructions as $ins) {
+        foreach ($ins as $i) {
+            $i == 'U' && !isAtEdge($keypad, $x, $y-1) ? ($y --) : null;
+            $i == 'R' && !isAtEdge($keypad, $x+1, $y) ? ($x ++) : null;
+            $i == 'D' && !isAtEdge($keypad, $x, $y+1) ? ($y ++) : null;
+            $i == 'L' && !isAtEdge($keypad, $x-1, $y) ? ($x --) : null;
+        }
+
+        $code .= $keypad[$y][$x];
+    }
+
+    return $code;
+}
+
 if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
-    $code = aoc2016day2();
+    $code = aoc2016day2part1();
+    $codeNewKeypad = aoc2016day2part2();
 
     line("1. The bathroom code is: $code.");
+    line("1. The bathroom code with new keypad is: $codeNewKeypad.");
 }
