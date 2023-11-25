9b3b1e6bdc10b0dee77718e74ac06337edb3db3e
Michael Goodman <bulgedition@gmail.com>
2023-11-25 22:56:37 +0200

    added description of part one for 2016-12-05

---
+++ 2016/day-05/solution.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Advent of Code 2016
+ * Day 5: How About a Nice Game of Chess?
+ *
+ * Part One
+ *
+ * @return string
+ */
+function aoc2016day5part1(): string
+{
+    return '';
+}
+
+
+if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
+    $password = aoc2016day5part1();
+
+    line("1. The number of possible triangles is: $password.");
+}
