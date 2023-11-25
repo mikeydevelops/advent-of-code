a0d14cc9fe7ebdd53bd0e06bf428d75dcd76a33c
Michael Goodman <bulgedition@gmail.com>
2023-11-25 21:25:45 +0200

    added solution of part two for 2016-12-03

--- 2016/day-03/solution.php
+++ 2016/day-03/solution.php
@@ -15,6 +15,31 @@ function getTriangles(): array
 }
 
 /**
+ * Count possible triangles from given list of triangle sides.
+ *
+ * @param  array  $triangles
+ * @return integer
+ */
+function countPossibleTriangles(array $triangles): int
+{
+    $possibilities = 0;
+
+    foreach ($triangles as $triangle) {
+        $p = [
+            $triangle[0] + $triangle[1] > $triangle[2],
+            $triangle[1] + $triangle[2] > $triangle[0],
+            $triangle[2] + $triangle[0] > $triangle[1],
+        ];
+
+        if ($p[0] && $p[1] && $p[2]) {
+            $possibilities ++;
+        }
+    }
+
+    return $possibilities;
+}
+
+/**
  * Advent of Code 2016
  * Day 3: Squares With Three Sides
  *
@@ -24,27 +49,36 @@ function getTriangles(): array
  */
 function aoc2016day3part1(): int
 {
+    return countPossibleTriangles(getTriangles());
+}
+
+/**
+ * Advent of Code 2016
+ * Day 3: Squares With Three Sides
+ *
+ * Part Two
+ *
+ * @return int
+ */
+function aoc2016day3part2(): int
+{
     $triangles = getTriangles();
 
-    $possibilities = 0;
+    $vertical = array_merge(
+        array_column($triangles, 0),
+        array_column($triangles, 1),
+        array_column($triangles, 2),
+    );
 
-    foreach ($triangles as $triangle) {
-        $pairs = [
-            $triangle[0] + $triangle[1] > $triangle[2],
-            $triangle[1] + $triangle[2] > $triangle[0],
-            $triangle[2] + $triangle[0] > $triangle[1],
-        ];
+    $vertical = array_chunk($vertical, 3);
 
-        if (count(array_unique($pairs)) == 1 && $pairs[0] === true) {
-            $possibilities ++;
-        }
-    }
-
-    return $possibilities;
+    return countPossibleTriangles($vertical);
 }
 
 if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
-    $code = aoc2016day3part1();
+    $possibilities = aoc2016day3part1();
+    $vertical = aoc2016day3part2();
 
-    line("1. The number of possible triangles is: $code.");
+    line("1. The number of possible triangles is: $possibilities.");
+    line("2. The number of possible vertical triangles is: $vertical.");
 }
