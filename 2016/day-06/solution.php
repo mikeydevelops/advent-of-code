d40c1cd735286b2b4cd5859acc67eefc0eae736e
Michael Goodman <bulgedition@gmail.com>
2023-11-26 19:56:31 +0200

    added solution of part two for 2016-12-06

--- 2016/day-06/solution.php
+++ 2016/day-06/solution.php
@@ -58,8 +58,37 @@ function aoc2016day6part1(): string
     return $corrected;
 }
 
+/**
+ * Advent of Code 2016
+ * Day 6: Signals and Noise
+ *
+ * Part Two
+ *
+ * @return string
+ */
+function aoc2016day6part2(): string
+{
+    $messages = getMessages(example: false);
+    $len = count($messages[0]);
+    $corrected = '';
+
+    for ($i = 0; $i < $len; $i ++) {
+        $ranks = array_count_values(array_column($messages, $i));
+
+        asort($ranks);
+
+        $chars = array_keys($ranks);
+
+        $corrected .= array_shift($chars);
+    }
+
+    return $corrected;
+}
+
 if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
     $corrected = aoc2016day6part1();
+    $actually = aoc2016day6part2();
 
     line("1. The corrected message is: $corrected.");
+    line("2. Actually, actually the corrected message is: $actually.");
 }
