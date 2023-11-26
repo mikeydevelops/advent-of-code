371ca8ffc3ff9e12bac8a69974a4fc8700150b07
Michael Goodman <bulgedition@gmail.com>
2023-11-26 19:35:27 +0200

    added description of part one for 2016-12-06

---
+++ 2016/day-06/solution.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Advent of Code 2016
+ * Day 6: Signals and Noise
+ *
+ * Part One
+ *
+ * @return int
+ */
+function aoc2016day6part1(): int
+{
+    return 0;
+}
+
+if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
+    $corrected = aoc2016day6part1();
+
+    line("1. The corrected message is: $corrected.");
+}
