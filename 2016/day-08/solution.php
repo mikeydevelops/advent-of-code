743947b42c5f9a10312f41494a0ba3f50387a697
Michael Goodman <bulgedition@gmail.com>
2023-11-26 21:15:21 +0200

    added description of part one for 2016-12-07

---
+++ 2016/day-08/solution.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Advent of Code 2016
+ * Day 8: Two-Factor Authentication
+ *
+ * Part One
+ *
+ * @return int
+ */
+function aoc2016day8part1(): int
+{
+    return 0;
+}
+
+if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
+    $litPixels = aoc2016day8part1();
+
+    line("1. The lit pixels are: $litPixels.");
+}
