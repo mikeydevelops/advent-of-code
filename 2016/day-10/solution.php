4f3606498ba4491df3c470cc27da95ca677b9ae6
Michael Goodman <bulgedition@gmail.com>
2023-11-27 20:39:20 +0200

    added description of part one for 2016-12-10

---
+++ 2016/day-10/solution.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Advent of Code 2016
+ * Day 10: Balance Bots
+ *
+ * Part One
+ *
+ * @return int
+ */
+function aoc2016day10part1(): int
+{
+    return 0;
+}
+
+if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
+    $botNumber = aoc2016day10part1();
+
+    line("1. The number of possible triangles is: $botNumber.");
+}
