2490855bcd601f46f2551fb1b8ff22ed8f13e4bc
Michael Goodman <bulgedition@gmail.com>
2024-04-28 23:17:55 +0300

    Added periodic table helper class, RTG and Microchip now show their element symbol instead of custom abbreviation

--- aoc/Year2016/Day11/Microchip.php
+++ aoc/Year2016/Day11/Microchip.php
@@ -4,8 +4,27 @@ namespace Mike\AdventOfCode\Year2016\Day11;
 
 class Microchip
 {
+    /**
+     * Create new instance of Radioisotope Thermoelectric Microchip
+     */
     public function __construct(public string $element)
     {
+        //
+    }
 
+    /**
+     * Convert the microchip to abbreviated string.
+     */
+    public function abbr(): string
+    {
+        return PeriodicTable::symbol($this->element) . 'M';
+    }
+
+    /**
+     * Convert the microchip to string.
+     */
+    public function __toString(): string
+    {
+        return "$this->element-compatible microchip";
     }
 }
