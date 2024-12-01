9b8129c6b402fe12d4933c370852fb1825957891
Mihail Ivanov <bulgedition@gmail.com>
2024-12-01 08:45:42 +0200

    added solution for day 1 in 2024

---
+++ aoc/Solutions/Year2024/Day01.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Mike\AdventOfCode\Solutions\Year2024;
+
+use Mike\AdventOfCode\Solutions\Solution;
+
+class Day01 extends Solution
+{
+    /**
+     * The example input to be used to test the solution.
+     */
+    protected ?string $exampleInput = <<<TXT
+    3   4
+    4   3
+    2   5
+    1   3
+    3   9
+    3   3
+    TXT;
+
+    /**
+     * Process the input from the challenge.
+     */
+    public function transformInput(string $input): mixed
+    {
+        $lists = preg_split('/\r?\n/', $input);
+
+        $lists = array_map(function ($row) {
+            $row = preg_split('/\s+/', trim($row));
+
+            return array_map('intval', $row);
+        }, $lists);
+
+        return [array_column($lists, 0), array_column($lists, 1)];;
+    }
+
+    /**
+     * Run the first part of the challenge.
+     */
+    public function part1()
+    {
+        $lists = $this->getInput();
+
+        sort($lists[0]); sort($lists[1]);
+
+        $distance = 0;
+
+        for ($i = 0; $i < count($lists[0]); $i++) {
+            $distance += $lists[0][$i] - $lists[1][$i];
+        }
+
+        return abs($distance);
+    }
+
+    /**
+     * Run the second part of the challenge.
+     */
+    public function part2()
+    {
+        $lists = $this->getInput();
+
+        $similarity = 0;
+
+        $vals = array_count_values($lists[1]);
+
+        for ($i = 0; $i < count($lists[0]); $i++) {
+            $id = $lists[0][$i];
+            $similarity += $id * ($vals[$id] ?? 0);
+        }
+
+        return $similarity;
+    }
+}
